d6a893d89913a35341033fe941f62001e17ee013
kakgonbri <buikhacbin@gmail.com>
2025-05-15 21:27:55 +0700

    react build in workflow

--- reactTest.Tests/UnitTest1.cs
+++ reactTest.Tests/UnitTest1.cs
@@ -2,6 +2,11 @@ using FluentAssertions;
 
 namespace reactTest.Tests
 {
+    /// <summary>
+    /// sadsakdaskdmaskmdas<br></br>
+    /// askdnaskdalda
+    /// <code>ssssssss</code>
+    /// </summary>
     public class UnitTest1
     {
         [Fact]
