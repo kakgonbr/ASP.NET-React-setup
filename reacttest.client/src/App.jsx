7d713bf655bc1f664007a6f409445f565ff2f1b1
kakgonbri <buikhacbin@gmail.com>
2025-05-15 13:16:30 +0700

    successful build trigger

--- reacttest.client/src/App.jsx
+++ reacttest.client/src/App.jsx
@@ -34,10 +34,10 @@ function App() {
     return (
         <div>
             <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server. Also, test6!!</p>
+            <p>This component demonstrates fetching data from the server. Also, test7!!</p>
             {contents}
         </div>
-    // );
+    );
     
     async function populateWeatherData() {
         const response = await fetch('weatherforecast');
