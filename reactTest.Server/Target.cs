c2b6496f0b58e422beb850c54e893b872e533b76
kakgonbri <buikhacbin@gmail.com>
2025-05-15 13:02:38 +0700

    added test

---
+++ reactTest.Server/Target.cs
@@ -0,0 +1,10 @@
+﻿namespace reactTest.Server
+{
+    internal class Target
+    {
+        public static bool doSomething()
+        {
+            return true;
+        }
+    }
+}
---
+++ reactTest.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+
+namespace reactTest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            reactTest.Server.Target.doSomething().Should().BeTrue();
+        }
+    }
+}
